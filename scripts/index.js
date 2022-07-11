5fa577d7a6d6c04034a079d7048b93ec846f026b
Olga-GSF <101978065+Olga-GSF@users.noreply.github.com>
2022-07-11 23:45:45 +0300

    Update index.js

--- scripts/index.js
+++ scripts/index.js
@@ -1,40 +1,127 @@
-const editButton = document.querySelector('.profile__button-edit')
-const popup = document.querySelector('.popup')
-const closePopupButton = document.querySelector('.popup__button-close')
+const cardTemplate = document.querySelector('#card-template').content;
+
+const buttonEditProfile = document.querySelector('.profile__button-edit')
+
+const profilePopup = document.querySelector('.popup_type_edit-profile')
+const cardPopup = document.querySelector('.popup_type_add-card')
+const imagePopup = document.querySelector('.popup_type_big-image')
+const popupElements = document.querySelectorAll('.popup')
+
+const buttonCloseProfile = profilePopup.querySelector('.popup__button-close')
+const buttonCloseCard = cardPopup.querySelector('.popup__button-close')
+const buttonCloseImage = imagePopup.querySelector('.popup__button-close')
+
+const buttonAddCard = document.querySelector('.profile__button-add')
+
 const titleElement = document.querySelector('.profile__title')
 const subtitleElement = document.querySelector('.profile__subtitle')
-const nameFieldElement = document.querySelector('.popup__input_name_firstname')
-const descriptionFieldElement = document.querySelector('.popup__input_name_description')
-const formElement = document.querySelector('.popup__form')
-/*const likedElement = document.querySelector('.button_type_like')*/
+const nameFieldElement = profilePopup.querySelector('.popup__input_name_firstname')
+const descriptionFieldElement = profilePopup.querySelector('.popup__input_name_description')
+
+const formElementProfile = profilePopup.querySelector('.popup__form')
+const formElementCard = cardPopup.querySelector('.popup__form_place_add')
+const cardsItemsElement = document.querySelector('.cards__items')
+const nameAddFieldElement = cardPopup.querySelector('.popup__input_name_place')
+const linkFieldElement = cardPopup.querySelector('.popup__input_name_link')
+
+const getCardByEvent = evt => evt.currentTarget.closest('.card');
+
+const buttonSubmitNewCard = formElementCard.querySelector('.popup__button-submit')
 
 function openPopup(popup) {
-  popup.classList.add('popup_is-open')
+  popup.classList.add('popup_is-open');
+  document.addEventListener('keydown', handlerEsc);
+  document.addEventListener('click', handleOverlay);
+};
+
+function openProfilePopup() {
   nameFieldElement.value = titleElement.textContent;
   descriptionFieldElement.value = subtitleElement.textContent;
-}
+  openPopup(profilePopup);
+};
 
 function closePopup(popup) {
-  popup.classList.remove('popup_is-open')
+  popup.classList.remove('popup_is-open');
+  document.removeEventListener('keydown', handlerEsc);
+  document.removeEventListener('click', handleOverlay);
+};
+
+function handlerEsc(evt) {
+  if (evt.key === 'Escape') {
+    popupElements.forEach(closePopup);
+  };
+};
+
+function handleOverlay(evt) {
+  if (evt.target.classList.contains('popup')) {
+    popupElements.forEach(closePopup);
+  };
 }
 
-function formEdit(event) {
+function editFormProfile(event) {
   event.preventDefault()
   titleElement.textContent = nameFieldElement.value;
   subtitleElement.textContent = descriptionFieldElement.value;
-  closePopup(popup)
+  closePopup(profilePopup);
+};
+
+function addFormCard(evt) {
+  evt.preventDefault()
+  const cardNew = {
+    name: nameAddFieldElement.value,
+    link: linkFieldElement.value
+  };
+  renderCard(cardNew);
+  formElementCard.reset();
+  closePopup(cardPopup);
+  buttonSubmitNewCard.classList.add('popup__button-submit_disabled');
+  buttonSubmitNewCard.setAttribute('disabled', true);
+};
+
+
+buttonEditProfile.addEventListener('click', () => openProfilePopup(profilePopup));
+buttonCloseProfile.addEventListener('click', () => closePopup(profilePopup));
+buttonCloseCard.addEventListener('click', () => closePopup(cardPopup));
+buttonCloseImage.addEventListener('click', () => closePopup(imagePopup));
+buttonAddCard.addEventListener('click', () => openPopup(cardPopup));
+formElementProfile.addEventListener('submit', editFormProfile);
+formElementCard.addEventListener('submit', addFormCard);
+
+function handleLikeButton(evt) {
+  evt.target.classList.toggle('button_is-active');
+};
+
+const imagePopupCard = document.querySelector('.popup__big-image')
+const imagePopupTitle = document.querySelector('.popup__image-title')
+
+function renderCard(element) {
+  const cardElement = createCard(element);
+  cardsItemsElement.prepend(cardElement);
+};
+
+function createCard(element) {
+  const cardElement = cardTemplate.querySelector('.card').cloneNode(true);
+  const bigImage = cardElement.querySelector('.card__image');
+  bigImage.addEventListener('click', () => openCard(element));
+
+  bigImage.src = element.link;
+  cardElement.querySelector('.card__title').textContent = element.name;
+  bigImage.alt = element.name;
+
+  cardElement.querySelector('.card__button-like').addEventListener('click', handleLikeButton);
+
+  cardElement.querySelector('.card__button-delete').addEventListener('click', evt => {
+    const card = getCardByEvent(evt);
+    card.remove();
+  });
+  return cardElement;
 }
 
-/*function liked(likedElement) {
-  likedElement.classList.add('botton_type_active')
-}*/
+initialCards.forEach(renderCard);
 
-editButton.addEventListener('click', () => openPopup(popup))
-
-closePopupButton.addEventListener('click', () => closePopup(popup))
-
-formElement.addEventListener('submit', formEdit)
-
-/*likedElement.addEventListener('click', function () {
-  likedElement.style = 'background-image: url(../../../images/UnionHeartLike.svg)';
-})*/
+function openCard(element) {
+  imagePopupCard.src = element.link;
+  imagePopupTitle.textContent = element.name;
+  imagePopupCard.alt = element.name;
+  openPopup(imagePopup);
+};
